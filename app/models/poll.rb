fb61edb5c92c3c6afaf2187baa08a6d047f2ee3f
Matteo Dauba <matteo.elyas@gmail.com>
2026-05-28 16:17:47 +0200

    Dylan nous sauve

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,11 +1,14 @@
 class Poll < ApplicationRecord
   has_many :conversations, dependent: :destroy
   belongs_to :user, optional: true
-  has_many :votes
+  has_many :votes, dependent: :destroy
 end
 
-# use this incase you want to delete a poll that already has votes 
+# use this incase you want to delete a poll that already has votes
 # class Poll < ApplicationRecord
 #   belongs_to :user, optional: true
 #   has_many :votes, dependent: :destroy
 # end
+
+
+# if poll belong to user show bin
