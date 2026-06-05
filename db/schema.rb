92ee0a464d197dd488c44704e1e17c4b44d22da1
Camille Parcigneau <camilleparcigneau@gmail.com>
2026-06-05 12:17:17 +0200

    fixingoptions

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[8.1].define(version: 2026_06_04_150312) do
-=======
 ActiveRecord::Schema[8.1].define(version: 2026_06_04_153335) do
->>>>>>> feature/globe-landing
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
