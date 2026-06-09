89b2f210bdca387b51418d1940695e01eb6827a9
rudy valace <rudyvalace@gmail.com>
2026-06-09 17:44:19 +0200

    feature/polls-explore

--- db/schema.rb
+++ db/schema.rb
@@ -131,6 +131,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_09_090348) do
     t.string "country"
     t.datetime "created_at", null: false
     t.datetime "expires_at"
+    t.string "image_url"
     t.float "lat"
     t.float "lon"
     t.string "title_question"
