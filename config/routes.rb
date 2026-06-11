c42a3466218deefba3907ffaca8e2f9846a90db5
fabian moreno <f.morenoaraque@yahoo.fr>
2026-06-11 11:46:40 +0200

    Product Checklist + cloudinary

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get "/globe", to: "globe#index"
 
   get "up" => "rails/health#show", as: :rails_health_check
+
+  match "/404", to: "errors#not_found",              via: :all
+  match "/500", to: "errors#internal_server_error",  via: :all
   resources :polls, only: [:index, :new, :create, :destroy, :show] do
     member do
       get :country_votes
